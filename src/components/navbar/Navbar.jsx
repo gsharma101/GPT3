1a30e7f06ff7ea7de5907a82556fa1fb7587bdba
gsharma101 <sharmag8121@gmail.com>
2022-12-29 21:13:29 +0530

    initial commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <div>
+      Features
+    </div> 
+  )
+}
+
+export default Navbar
---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export {default as Blog } from './blog/Blog';
+export {default as features } from './features/Features';
+export {default as footer } from './footer/Footer';
+export {default as header } from './header/Header';
+export {default as possibility } from './possibility/Possibility';
+export {default as whatGPT3 } from './whatGPT3/WhatGPT3';
