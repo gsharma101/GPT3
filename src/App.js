2e65660c0ff0eaaffde4bdc717436dec9ab72ea0
gsharma101 <sharmag8121@gmail.com>
2022-12-30 08:08:17 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
-import {CTA, Brand, Navbar} from './components';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css'
 
 
 const App = () => {
   return (
-    <div>
-        <h1>Work Like Hell</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
- 
+
 export default App
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
 import './navbar.css';
+
+// ? BEM Block Element Modifier
 
 const Navbar = () => {
   return (
-    <div>
-      Features
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
+          <img src={logo} alt="logo"/>
+        </div>
+        <div className='gpt3__navbar-links_container'>
+          <p><a href="#home">Home</a></p>
+          <p><a href="#wgpt3">What is GPT?</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
     </div> 
   )
 }
